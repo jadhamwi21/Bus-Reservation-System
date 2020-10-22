92220980283cfb5b51cf36a008cdae91504a2a79
Jad Al-hamwi <jad.hamwi8@hotmail.com>
2020-10-22 17:16:34 +0300

    initial commit

---
+++ Bus Reservation System.cpp
@@ -0,0 +1,259 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+using namespace std;
+void Differentiation()
+{
+	cout << "--------------------------------" << endl;
+}
+//A Structure For The Buses That's Gonna Be Installed to The Bus System
+struct Resturaunt
+{
+	string Name;
+	string Location;
+	std::map<string, string> Menu;
+};
+struct Bus
+{
+	long BusNo;
+	string Drivername;
+	string ArrivalTime;
+	string DepartureTime;
+	string TransporationLine;
+	std::vector<Resturaunt> Resturaunts;
+	string Destination;
+	string Seats[32];
+};
+//Class For Bus_System Including Function To Represent GUI into the User
+class Bus_System
+{
+private:
+	//Vector For Storing Buses for a certain Bus_System(ex: Bus_System BTest)
+	std::vector<Bus> Buses;
+
+public:
+	//Here I Did Make A Function To Make The Bus System Interface Shows Up On The Screen And Ask
+	//The User For Inputing An Option(1 -> 5)
+	void
+		Bus_System_run()
+	{
+		while (true)
+		{
+			cout << "1.Install Bus" << endl;
+			cout << "2.Reservation" << endl;
+			cout << "3.Show Reservation Infos" << endl;
+			cout << "4.Buses Available" << endl;
+			cout << "5.Exit" << endl;
+			cout << "Enter Your Choice Sir : ";
+			int n;
+			//User's Choice
+			cin >> n;
+			system("cls");
+			//This Switch Is Written To Apply The Function That Makes The Purpose Of The User
+			switch (n)
+			{
+			case 1:
+				InstallBusInformation();
+				Differentiation();
+				break;
+			case 2:
+				Reservation();
+				Differentiation();
+				break;
+			case 3:
+				showReservationInfos();
+				Differentiation();
+				break;
+			case 4:
+				ShowAvailableBuses();
+				Differentiation();
+				break;
+			case 5:
+				cout << "Program Terminated" << endl;
+				exit(1);
+			default:
+				cout << "Error With The Input" << endl;
+				break;
+			}
+		}
+	}
+	//Start Of BusInstallation
+	void InstallBusInformation()
+	{
+		Bus Temp;
+		cout << "Bus Number : ";
+		cin >> Temp.BusNo;
+		while (std::find_if(Buses.begin(), Buses.end(), [=](Bus temp) {
+			return Temp.BusNo == temp.BusNo;
+			}) != Buses.end())
+		{
+			cout << "Sorry , Please Enter An Unused Bus Number" << endl;
+			cin >> Temp.BusNo;
+		}
+			cout << "Driver's Name : ";
+			cin >> Temp.Drivername;
+			cout << "Arrival Time : ";
+			cin >> Temp.ArrivalTime;
+			cout << "Departure Time : ";
+			cin >> Temp.DepartureTime;
+			cout << "From : ";
+			cin >> Temp.Destination;
+			string s;
+			cout << "To : ";
+			cin >> s;
+			Temp.Destination += " " + s;
+			Temp.TransporationLine = Temp.Destination.substr(0, Temp.Destination.find(" ")) + " And " + Temp.Destination.substr(Temp.Destination.find(" ") + 1, Temp.Destination.size());
+			std::fill_n(Temp.Seats, 32, "Empty");
+			Buses.push_back(Temp);
+	}
+	//End Of BusInstallation
+	//Start Of Reservation
+	void Reservation()
+	{
+		if (Buses.empty())
+		{
+			cout << "No Installed Buses Yet" << endl;
+			return;
+		}
+		long BusNumber;
+		while (true)
+		{
+			if (Buses.empty())
+			{
+				cout << "No Installed Buses Yet" << endl;
+				break;
+			}
+			bool flag = false;
+			cout << "Enter Bus Number : ";
+			cin >> BusNumber;
+			std::vector<Bus>::iterator it = Buses.begin();
+			for (it; it != Buses.end(); it++)
+			{
+				if (it->BusNo == BusNumber)
+				{
+					flag = true;
+					int BusSeat;
+					while (true)
+					{
+						cout << "Enter Seats Number : ";
+						cin >> BusSeat;
+						if (BusSeat <= 0 || BusSeat > 32)
+						{
+							cout << "Enter A Valid Number Please ! " << endl;
+							continue;
+						}
+						else if (it->Seats[BusSeat - 1] != "Empty")
+						{
+							cout << "It's Reserved by " + it->Seats[BusSeat - 1] << endl;
+							continue;
+						}
+						else
+						{
+							string name;
+							cout << "Enter Your Name : ";
+							cin.ignore();
+							getline(cin, name);
+							it->Seats[BusSeat - 1] = name;
+							break;
+						}
+					}
+				}
+			}
+			if (flag == false)
+			{
+				cout << "Wrong Number" << endl;
+			}
+			else
+			{
+				break;
+			}
+		}
+	};
+	//End Of Reservation
+	void showReservationInfos()
+	{
+		if (Buses.empty())
+		{
+			cout << "No Installed Buses Yet" << endl;
+		}
+		cout << "Enter Bus Number : ";
+		long BusNumber;
+		cin >> BusNumber;
+		std::vector<Bus>::iterator it = Buses.begin();
+		bool flag = false;
+		for (it; it != Buses.end(); it++)
+		{
+			if (it->BusNo == BusNumber)
+			{
+				flag = true;
+
+				cout << "Bus Number : " << it->BusNo << " | ";
+				cout << "Driver : " << it->Drivername << endl;
+				cout << "Arrival Time : " << it->ArrivalTime << " | ";
+				cout << "Deprature Time : " << it->DepartureTime << endl;
+				string From = it->Destination.substr(0, it->Destination.find(" "));
+				cout << "From : " << From << " || ";
+				string To = it->Destination.substr(it->Destination.rfind(" "), it->Destination.length());
+				cout << "To : " << To << endl;
+
+				int count = 0;
+				std::map<int, string> Connections;
+				for (int i = 0; i < 32; i++)
+				{
+					if (it->Seats[i] == "Empty")
+					{
+						count++;
+					}
+					else
+					{
+						Connections.insert(pair<int, string>(i + 1, it->Seats[i]));
+					}
+					cout << setw(5) << to_string(i + 1) << ".  " << setw(9) << it->Seats[i];
+					if ((i + 1) % 4 == 0)
+					{
+						cout << endl;
+					}
+				}
+				cout << endl;
+				cout << "There Are " + to_string(count) + " Seats Remaining Empty " << endl;
+				std::map<int, string>::iterator it = Connections.begin();
+				for (it; it != Connections.end(); it++)
+				{
+					cout << "The Seat Number " + to_string(it->first) + " Is Reserved For " + it->second << endl;
+				}
+			}
+		}
+		if (flag == false)
+		{
+			cout << "Number Isn't Right Please Try Again" << endl;
+			showReservationInfos();
+		}
+	}
+	void ShowAvailableBuses()
+	{
+		std::vector<Bus>::iterator it = Buses.begin();
+		for (it; it != Buses.end(); it++)
+		{
+			cout << "Bus Number : " << it->BusNo << " | ";
+			cout << "Driver : " << it->Drivername << endl;
+			cout << "Arrival Time : " << it->ArrivalTime << " | ";
+			cout << "Deprature Time : " << it->DepartureTime << endl;
+			string From = it->Destination.substr(0, it->Destination.find(" "));
+			cout << "From : " << From << " || ";
+			string To = it->Destination.substr(it->Destination.rfind(" "), it->Destination.length());
+			cout << "To : " << To << endl;
+		}
+	}
+};
+int main()
+{
+	Bus_System Test;
+	Test.Bus_System_run();
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Bus Reservation System
+### Description:
+__Bus Reservation System Software : It represents a pretty basic idea of reserving your seat on buses through the entire country, it's not going to benefit you in real life but it's just an idea that can be implemented in a much neater way__
+___
+### Table of Contents:
+* [How to use](#how-to-use)
+* [Demonstration](#demonstration)
+___
+### How to use
+Here Is Instructions How to use this project properly:
+1. Install The Buses :
+   * Enter Bus Number
+   * Driver's Name
+   * Arrival and Departure Time
+   * Direction
+2. Reserving Your Seat:
+    * Enter Bus Number
+    * Enter Seat Number
+    * Your Full Name
+3. Show Reservation Inforamtion :
+In case you wanted to check Bus's status you can choose this option and enter the Bus number you're willing to __reserve__ in.
+4. Show Available Buses : 
+This option for checking what buses are available and what direction they're heading.
+---
+### Demonstration
+
+> Interface :
+
+<img src="https://i.ibb.co/56cGv8Q/Screenshot-1.png" alt="Screenshot-1" border="0">
+
+> Install Bus : 
+
+<img src="https://i.ibb.co/M6yHfV8/Screenshot-2.png" alt="Screenshot-2" border="0">
+
+> Reservation :
+
+<img src="https://i.ibb.co/cyJscKh/Screenshot-3.png" alt="Screenshot-3" border="0">
+
+> Reservation Infos :
+
+<img src="https://i.ibb.co/7rW0Ctv/Screenshot-4.png" alt="Screenshot-4" border="0">
+
+> Available Buses :
+
+<img src="https://i.ibb.co/6m6skj9/Screenshot-5.png" alt="Screenshot-5" border="0">
+
+---
+# Note :
+In case you don't want to recompile the code or create a new project in order to run the program, you can simply launch<br>__the Executable File__:
+> Bus Reservation System.exe
